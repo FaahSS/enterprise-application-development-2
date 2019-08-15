f0c5e30dc238709e825db9b5094e0fad4688031e
Fabio Salgarella <faahsalgarella@gmail.com>
2019-08-15 09:48:49 -0300

    Introdução ao Asp.Net

---
+++ 03-Fiap.Web.AspNet/03-Fiap.Web.AspNet/Controllers/ClienteController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Fiap.Web.AspNet.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fiap.Web.AspNet.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet] //Abrir a página do formulário
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+        [HttpPost] //Cadastrar no clique do botão
+        public IActionResult Cadastrar(Cliente cliente)
+        {
+            ViewData["nome"] = cliente.Nome;
+            ViewBag.cpf = cliente.Cpf;
+            TempData["msg"] = "Usuário cadastrado";
+            return View();
+            // return Content(cliente.Nome + " " + cliente.Cpf);
+        }
+    }
+}

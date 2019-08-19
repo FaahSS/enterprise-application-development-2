35ed4b7cad4fba0d54fa5a02bf785f5c5f5f812a
Fabio Salgarella <faahsalgarella@gmail.com>
2019-08-19 09:11:36 -0300

    Exercício 2 - Utilizando os conceitos de ViewData e ViewBag

---
+++ 04-Fiap.Web.AspNet/04-Fiap.Web.AspNet/Controllers/UsuarioController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Fiap.Web.AspNet.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _04_Fiap.Web.AspNet.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Usuario usuario)
+        {
+            ViewData["nome"] = usuario.Nome;
+            ViewBag.dataN = usuario.DataNascimento;
+            ViewBag.email = usuario.Email;
+            TempData["msg"] = "Usuário Cadastrado";
+            //Retorna o objeto para view
+            return View(usuario);
+        }
+
+
+    }
+}
